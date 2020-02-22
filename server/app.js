164a71d92e0f162635d2aef585a483c8e873ded3
zhangzhano <xy199726@gmail.com>
2020-02-22 14:52:35 +0800

    用cors解决跨域问题

--- server/app.js
+++ server/app.js
@@ -1,13 +1,25 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const session = require('express-session')
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const productsRouter = require('./routes/product');
 
-var app = express();
+const app = express();
+
+// 配置 cors 解决跨域问题 
+app.all('*', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", req.headers.origin); //需要显示设置来源
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Credentials", true); //带cookies7     
+  res.header("Content-Type", "application/json;charset=utf-8");
+  next();
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +31,22 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  ////这里的name值得是cookie的name，默认cookie的name是：connect.sid
+  //name: 'hhw',
+  secret: 'zhaozhaozhaozhaozhaozhao',
+  cookie: {
+    maxAge: 1000*60*60
+  },
+  //重新保存：强制会话保存即使是未修改的。默认为true但是得写上
+  resave: false,
+  //强制“未初始化”的会话保存到存储。 
+  saveUninitialized: true,
+
+}))
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/products', productsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
