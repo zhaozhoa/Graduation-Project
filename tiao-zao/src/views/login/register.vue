935c07aa92e3ce1511610742b7bd39ff6fc357fb
zhangzhano <xy199726@gmail.com>
2020-02-22 15:01:47 +0800

    注册页面

--- tiao-zao/src/views/login/register.vue
+++ tiao-zao/src/views/login/register.vue
@@ -1,8 +1,18 @@
 <template>
   <div class="reg resetbg">
-    <img src="./../../assets/login/title.png" alt class="titlePic" />
-    <p class="title">注册</p>
-    <img src="./../../assets/login/line.png" alt class="icon" />
+    <img
+      src="./../../assets/login/title.png"
+      alt
+      class="titlePic"
+    >
+    <p class="title">
+      注册
+    </p>
+    <img
+      src="./../../assets/login/line.png"
+      alt
+      class="icon"
+    >
 
     <van-cell-group>
       <van-field
@@ -44,17 +54,30 @@
         @input="checkPhone"
       />
     </van-cell-group>
-    <van-button type="primary" size="large" class="btn" @click="regrister" :disabled="isClick">注册</van-button>
+    <van-button
+      type="primary"
+      size="large"
+      class="btn"
+      :disabled="isClick"
+      @click="regrister"
+    >
+      注册
+    </van-button>
   </div>
 </template>
 
 <script>
-import qs from "qs";
-// import md5 from 'js-md5'
 import { Field, Button, Cell, CellGroup } from "vant";
 
 export default {
   name: "Regrister",
+
+  components: {
+    [Field.name]: Field,
+    [Button.name]: Button,
+    [Cell.name]: Cell,
+    [CellGroup.name]: CellGroup
+  },
   data() {
     return {
       username: "",
@@ -72,13 +95,6 @@ export default {
     };
   },
 
-  components: {
-    [Field.name]: Field,
-    [Button.name]: Button,
-    [Cell.name]: Cell,
-    [CellGroup.name]: CellGroup
-  },
-
   computed: {},
 
   mounted() {},
@@ -89,9 +105,9 @@ export default {
         this.usernameTips = "请输入5~20个字符，包含字母或数字！";
         return;
       } else {
-        let { data: res } = await this.axios.post(
-          this.httpurl+"/enterprise/checkLoginName",
-          qs.stringify({ LOGIN_NAME: this.username, tm: new Date().getTime() })
+        
+        let { data: res } = await this.$api.userApi.hasUserName(
+          {userName: this.username}
         );
         if (res.code == 0) {
           this.usernameTips = "";
@@ -138,14 +154,14 @@ export default {
         this.phone !== "" &&
         this.checkUserName
       ) {
-        let { data: res } = await this.axios.post(
-          this.httpurl+"/enterprise/register",
-          qs.stringify({
-            LOGIN_NAME: this.username,
-            PASS_WORD: this.md5((this.password)),
-            TELEPHONE: this.phone,
+
+        let { data: res } = await this.$api.userApi.register(
+          {
+            userName: this.username,
+            password: this.md5((this.password)),
+            phone: this.phone,
             TYPE: 2
-          })
+          }
         );
         if (res.code == 0) {
           this.$router.push({ name: "registerSuccess" });
@@ -161,9 +177,8 @@ export default {
 </script>
 <style lang='less' scoped>
 .reg {
+  box-sizing: border-box;
   padding: 0.97rem;
-  // width: 100vw;
-  // height: 100vh;
   height: 100%;
   background: #fff;
   .titlePic {
