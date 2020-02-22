be76e375aa3e0cc0dcf333c4cada08c210b3405c
zhangzhano <xy199726@gmail.com>
2020-02-22 14:58:21 +0800

    axios封装

--- tiao-zao/src/request/http.js
+++ tiao-zao/src/request/http.js
@@ -25,14 +25,14 @@ const tip = msg => {
  * 跳转登录页
  * 携带当前页面路由，以期在登录页面完成登录后返回当前页面
  */
-const toLogin = () => {
-  router.replace({
-    path: '/login',
-    query: {
-      redirect: router.currentRoute.fullPath
-    }
-  });
-}
+// const toLogin = () => {
+//   router.replace({
+//     path: '/login',
+//     query: {
+//       redirect: router.currentRoute.fullPath
+//     }
+//   });
+// }
 
 /** 
  * 请求失败后的错误统一处理 
