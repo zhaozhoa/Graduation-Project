ca92dbb145348ad5c1490274be48a7526476135f
zhangzhano <xy199726@gmail.com>
2020-02-22 14:55:18 +0800

    用户模块请求的后端接口文件

---
+++ server/models/user.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+let Schema = mongoose.Schema
+
+let userSchema = new Schema({
+  phone: {
+    type: Number,
+    required: true
+  },
+  userName: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  createdTime: {
+    type: Date,
+    // 当 new Model 的时候，如果你没有传递 create_time,则 mongoose 会调用 default 的指定 Date.now 方法的返回值作为默认值
+    default: Date.now
+  },
+  lastModifiedTime: {
+    type: Date,
+    default: Date.now
+  },
+  gender: {
+    type: Number,
+    // -1保密，0男，1女
+    enum: [-1, 0, 1], //限定
+    default: -1
+  }
+})
+
+module.exports = mongoose.model('User', userSchema)
