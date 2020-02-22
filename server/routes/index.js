80893dc2105e919d667eee1544c775cb59c4240c
zhangzhano <xy199726@gmail.com>
2020-02-22 14:55:56 +0800

    添加了mongoose的连接

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const mongoose = require('mongoose')
+
+//  连接 mongoodb 数据库
+mongoose.connect('mongodb://localhost/tiaoZao', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+mongoose.connection.on('connected', () => {
+  console.log('MongoDB connected success');
+})
+mongoose.connection.on('error', () => {
+  console.log('Mongoose connected fail');
+})
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose connected disconnected')
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
