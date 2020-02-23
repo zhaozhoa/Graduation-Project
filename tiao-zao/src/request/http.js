23fc6bd44d3dad99df468bf221e4fbdc6c2f6d6c
zhangzhano <xy199726@gmail.com>
2020-02-23 14:01:00 +0800

    添加了拦截器，对后端返回的错误信息进行统一处理

--- tiao-zao/src/request/http.js
+++ tiao-zao/src/request/http.js
@@ -25,44 +25,47 @@ const tip = msg => {
  * 跳转登录页
  * 携带当前页面路由，以期在登录页面完成登录后返回当前页面
  */
-// const toLogin = () => {
-//   router.replace({
-//     path: '/login',
-//     query: {
-//       redirect: router.currentRoute.fullPath
-//     }
-//   });
-// }
+const toLogin = () => {
+  router.replace({
+    path: '/login',
+    query: {
+      redirect: router.currentRoute.fullPath
+    }
+  });
+}
 
 /** 
  * 请求失败后的错误统一处理 
  * @param {Number} status 请求失败的状态码
  */
-// const errorHandle = (status, other) => {
-//   // 状态码判断
-//   switch (status) {
-//     // 401: 未登录状态，跳转登录页
-//     case 401:
-//       toLogin();
-//       break;
-//       // 403 token过期
-//       // 清除token并跳转登录页
-//     case 403:
-//       tip('登录过期，请重新登录');
-//       localStorage.removeItem('token');
-//       store.commit('loginSuccess', null);
-//       setTimeout(() => {
-//         toLogin();
-//       }, 1000);
-//       break;
-//       // 404请求不存在
-//     case 404:
-//       tip('请求的资源不存在');
-//       break;
-//     default:
-//       console.log(other);
-//   }
-// }
+const errorHandle = (status, other) => {
+  switch (status) {
+    // 401: 未登录状态，跳转登录页
+    case 401:
+      toLogin();
+      break;
+      // 403 token过期
+      // 清除token并跳转登录页
+    case 403:
+      tip('登录过期，请重新登录');
+      localStorage.removeItem('token');
+      store.commit('islogin', null);
+      setTimeout(() => {
+        toLogin();
+      }, 1000);
+      break;
+      // 404请求不存在
+    case 404:
+      tip('请求的资源不存在');
+      break;
+      // 500 服务器出错
+      case 500:
+        tip('服务器繁忙，请稍后重试');
+      break;
+    default:
+      tip(other);
+  }
+}
 
 // 创建axios实例
 var instance = axios.create({
@@ -70,7 +73,6 @@ var instance = axios.create({
 });
 // 设置post请求头
 instance.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-
 
 // /** 
 //  * 请求拦截器 
@@ -88,30 +90,20 @@ instance.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlenco
 //   },
 //   error => Promise.error(error))
 
-// // 响应拦截器
-// instance.interceptors.response.use(
-//   // 请求成功
-//   res => res.status === 200 ? Promise.resolve(res) : Promise.reject(res),
-//   // 请求失败
-//   error => {
-//     const {
-//       response
-//     } = error;
-//     if (response) {
-//       // 请求已发出，但是不在2xx的范围 
-//       errorHandle(response.status, response.data.message);
-//       return Promise.reject(response);
-//     } else {
-//       // 处理断网的情况
-//       // eg:请求超时或断网时，更新state的network状态
-//       // network状态在app.vue中控制着一个全局的断网提示组件的显示隐藏
-//       // 关于断网组件中的刷新重新获取数据，会在断网组件中说明
-//       if (!window.navigator.onLine) {
-//         store.commit('changeNetwork', false);
-//       } else {
-//         return Promise.reject(error);
-//       }
-//     }
-//   });
+// 响应拦截器
+instance.interceptors.response.use(
+  // 请求成功
+  res => res.status >= 200 && res.status<300 || res.status === 304 ? Promise.resolve(res) : Promise.reject(res),
+  // 请求失败
+  error => {
+    const {
+      response
+    } = error;
+    if (response) {
+      // 请求已发出，但是不在2xx的范围 
+      errorHandle(response.status, response.data.msg);
+      return Promise.reject(response);
+    } 
+  });
 
 export default instance;
