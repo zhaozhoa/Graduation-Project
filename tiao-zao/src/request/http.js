8a41ef61cf388e6d1a6a591e42a5e60ab59ac005
zhangzhano <xy199726@gmail.com>
2020-02-24 00:38:31 +0800

    添加了请求拦截器，请求携带token

--- tiao-zao/src/request/http.js
+++ tiao-zao/src/request/http.js
@@ -42,13 +42,17 @@ const errorHandle = (status, other) => {
   switch (status) {
     // 401: 未登录状态，跳转登录页
     case 401:
-      toLogin();
+      tip('您还没有登陆，请先登陆')
+      setTimeout(() => {
+        toLogin();
+        
+      }, 1000);
       break;
       // 403 token过期
       // 清除token并跳转登录页
     case 403:
       tip('登录过期，请重新登录');
-      localStorage.removeItem('token');
+      sessionStorage.removeItem('token');
       store.commit('islogin', null);
       setTimeout(() => {
         toLogin();
@@ -74,21 +78,23 @@ var instance = axios.create({
 // 设置post请求头
 instance.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
-// /** 
-//  * 请求拦截器 
-//  * 每次请求前，如果存在token则在请求头中携带token 
-//  */
-// instance.interceptors.request.use(
-//   config => {
-//     // 登录流程控制中，根据本地是否存在token判断用户的登录情况        
-//     // 但是即使token存在，也有可能token是过期的，所以在每次的请求头中携带token        
-//     // 后台根据携带的token判断用户的登录情况，并返回给我们对应的状态码        
-//     // 而后我们可以在响应拦截器中，根据状态码进行一些统一的操作。        
-//     const token = store.state.token;
-//     token && (config.headers.Authorization = token);
-//     return config;
-//   },
-//   error => Promise.error(error))
+/** 
+ * 请求拦截器 
+ * 每次请求前，如果存在token则在请求头中携带token 
+ */
+instance.interceptors.request.use(
+  config => {
+    // 登录流程控制中，根据本地是否存在token判断用户的登录情况        
+    // 但是即使token存在，也有可能token是过期的，所以在每次的请求头中携带token        
+    // 后台根据携带的token判断用户的登录情况，并返回给我们对应的状态码        
+    // 而后我们可以在响应拦截器中，根据状态码进行一些统一的操作。
+
+    if (sessionStorage.getItem('token')) {
+      config.headers.Authorization = 'Bearer ' + JSON.parse(sessionStorage.getItem('token')).value
+    }
+    return config;
+  },
+  error => Promise.error(error))
 
 // 响应拦截器
 instance.interceptors.response.use(
