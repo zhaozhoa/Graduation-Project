42da6b374f74142d7d827e9c3b4ba2daa65eec4e
zhangzhano <xy199726@gmail.com>
2020-02-23 14:02:23 +0800

    产品的路由处理

---
+++ server/routes/product.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const products = require('../models/products')
+
+
+
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
